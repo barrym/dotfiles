a2ea77eaf31337a8274c17eb89a247cd13b6fd5d
Barry Mitchelson <barry.mitchelson@gmail.com>
2012-09-06 10:51:27 +0100

    Fixed install.rb symlink issue.

--- install.rb
+++ install.rb
@@ -7,5 +7,6 @@ home = File.expand_path('~')
 Dir['etc/*'].each do |file|
   next if file =~ /install|readme/i
   target = File.join(home, ".#{File.basename(file)}")
-  puts `ln -svf #{File.expand_path file} #{target}`
+  puts "Linking #{File.expand_path file} to #{target}"
+  puts `ln -svfh #{File.expand_path file} #{target}`
 end
