38824550b6eff3fa57df8155716e484fb2e9ef20
Barry Mitchelson <bmitchelson@google.com>
2014-08-11 12:52:05 +0100

    Update symlinking in install.rb to work with Linux and OSX. Also prevents directory symlinking weirdness.

--- install.rb
+++ install.rb
@@ -8,5 +8,5 @@ Dir['etc/*'].each do |file|
   next if file =~ /install|readme/i
   target = File.join(home, ".#{File.basename(file)}")
   puts "Linking #{File.expand_path file} to #{target}"
-  puts `ln -svfh #{File.expand_path file} #{target}`
+  puts `ln -svfn #{File.expand_path file} #{target}`
 end
